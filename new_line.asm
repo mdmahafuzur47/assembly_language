d0663743b373ef7cb3947f8eeed11de03e6df1ca
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2025-06-11 16:09:13 +0600

    update new_line.asm

---
+++ new_line.asm
@@ -0,0 +1,27 @@
+.model small
+.stack 100h
+.data
+
+.code
+main proc
+    mov ah,1
+    int 21h
+    mov bl, al
+
+    ;new line
+    mov ah, 2
+    mov dl, 10 ; ASCII code for new line feed
+    int 21h
+    mov dl, 13 ; ASCII code for carriage return
+    int 21h
+
+    mov ah, 2
+    mov dl, bl
+    int 21h
+
+
+    exit:
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
